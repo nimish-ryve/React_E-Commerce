2961f32d131aae1a7d9eaeeb128612d9e4f2ec2a
Nimish GS <nimishgs@Nimishs-MacBook-Air.local>
2025-06-08 03:16:10 +0530

    Another commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -85,19 +85,19 @@ const Products = () => {
             className="btn btn-outline-dark btn-sm m-2"
             onClick={() => filterProduct("men's clothing")}
           >
-            Clothing for men
+            Men's Clothing
           </button>
           <button
             className="btn btn-outline-dark btn-sm m-2"
             onClick={() => filterProduct("women's clothing")}
           >
-            Clothing for women
+            Women's Clothing
           </button>
           <button
             className="btn btn-outline-dark btn-sm m-2"
             onClick={() => filterProduct("jewelery")}
           >
-            Jewelry
+            Jewelery
           </button>
           <button
             className="btn btn-outline-dark btn-sm m-2"
