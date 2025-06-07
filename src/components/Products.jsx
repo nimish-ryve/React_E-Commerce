8a0938099530527d949e26593e266bf0a2325dab
Nimish GS <nimishgs@Nimishs-MacBook-Air.local>
2025-06-07 23:42:08 +0530

    Changed text

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -79,7 +79,7 @@ const Products = () => {
             className="btn btn-outline-dark btn-sm m-2"
             onClick={() => setFilter(data)}
           >
-            All categories
+            All
           </button>
           <button
             className="btn btn-outline-dark btn-sm m-2"
@@ -126,7 +126,7 @@ const Products = () => {
                     {product.title.substring(0, 12)}...
                   </h5>
                   <p className="card-text">
-                    {product.description.substring(0, 70)}...
+                    {product.description.substring(0, 80)}...
                   </p>
                 </div>
                 <ul className="list-group list-group-flush">
